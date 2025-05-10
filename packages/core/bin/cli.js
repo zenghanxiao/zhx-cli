9cc77ad7a761db5d462c6c544326f0e8be5f85ee
zenghanxiao <zengxiao505685546@qq.com>
2025-05-10 18:44:04 +0800

    feat: 创建项目

---
+++ packages/core/bin/cli.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import { sum } from '@zhx-cli/utils';
+
+console.log('hello zhx-cli core');
+
+const res = sum(1, 2);
+
+console.log(res);
---
+++ packages/core/lib/index.js
@@ -0,0 +1,5 @@
+export default function () {
+  console.log('zhx-cli core');
+}
+
+console.log('zhx core index');
